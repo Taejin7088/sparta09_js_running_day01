097d3d84a0a24e7d4c3139666c1b19e556965daf
Taejin7088 <gog70888@gmail.com>
2025-01-06 18:22:05 +0900

    Update 06.js

--- 06.js
+++ 06.js
@@ -30,6 +30,6 @@ console.log("userBCart할인함수적용 ", userBCart)
 2.	1번의 결과에 대한 이유를 설명해보세요.
 applyCoupon 함수는 coupon의 discount 객체만큼 userACart.items.price를 5000원씩 깍는 함수임
 const userBCart = userACart; 는 두개의 주소값을 같다하는 선언문임으로 두개는 같음
-둘다깍임
+둘다깎임
 3.	원래 의도대로라면 유저 A와 유저 B 장바구니가 독립적으로 동작해야 하는데, 그렇게 하려면 코드를 어떻게 수정해야 할까요? 하드코딩하는방법밖에 모르겠음
 */
